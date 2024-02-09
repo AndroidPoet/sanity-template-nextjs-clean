8cc9cce58f55c932dd29b2cf346165f3b08e075d
Ranbir Singh <poetdroid2@gmail.com>
2024-02-09 12:20:45 +0530

    update

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -6,7 +6,7 @@ import author from './author'
 import category from './category'
 import blockContent from './blockContent'
 
-export const schemaTypes = [post,author,category,blockContent]
+export const schemaTypes = [post,author,blockContent]
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [post,author,category,blockContent],
 }
